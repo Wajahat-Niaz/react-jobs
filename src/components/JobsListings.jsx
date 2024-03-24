18c204e4999e2f5f15e401017dbf49863d6dd2dd
Wajahat <wajahatniaz2@gmail.com>
2024-03-24 16:52:03 +0400

    Part 1

---
+++ src/components/JobsListings.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import JobListing from './JobListing'
+import Jobs from '../jobs.json'
+
+const JobListings = () => {
+    const featuredJobs = Jobs.slice(0, 3)
+
+    return (
+        <><section className="bg-blue-50 px-4 py-10">
+            <div className="container-xl lg:container m-auto">
+                <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+                    Browse Jobs
+                </h2>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    {featuredJobs.map((job) => (
+                        <JobListing
+                            key={job.id}
+                            id={job.id}
+                            type={job.type}
+                            title={job.title}
+                            description={job.description}
+                            salary={job.salary}
+                            location={job.location}
+                        />
+                    ))}
+                </div>
+            </div>
+        </section></>
+    )
+}
+
+export default JobListings
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero'
+import JobsListings from '../components/JobsListings'
+import ViewAllJobs from '../components/ViewAllJobs'
+
+const HomePage = () => {
+    return (
+        <>
+            <Hero />
+            <JobsListings />
+            <ViewAllJobs />
+        </>
+    )
+}
+
+export default HomePage
